f2d7f31b7dd180296c322b5d288f8684b2eb9cb5
Бобрович Сергей Александрович <sbobrovich@clevertec.ru>
2024-04-27 09:35:58 +0300

    add kubernetes

--- src/main/java/com/example/weather/WeatherApplication.java
+++ src/main/java/com/example/weather/WeatherApplication.java
@@ -1,13 +1,14 @@
 package com.example.weather;
 
+import com.example.prometeuscustomstarter.annotation.EnableMetrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.TcpClient;
 
+@EnableMetrics
 @SpringBootApplication
 public class WeatherApplication {
 
@@ -17,10 +18,9 @@ public class WeatherApplication {
 
     @Bean
     public WebClient webClientWithTimeout() {
-        final var tcpClient = TcpClient.create();
 
         return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()))
                 .build();
     }
 
